32c937cdf3fcf0c7506bcde5c39f95c65c98643f
Kathryn Spahn <kathrynspahn@88-e9-fe-4e-d3-d5.wlan.lsu.edu>
2019-10-10 11:58:31 -0500

    Assignment 6

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let bg1;
+let bg2;
+let bg3;
+let mid1;
+let mid2;
+let mid3;
+let fr1;
+let fr1;
+let fr2;
+
+
+function setup() {
+    createCanvas(1000, 700);
+
+    bg1= loadImage('images/fish.jpg');
+    bg2= loadImage('images/floral.jpg');
+    bg3= loadImage('images/rainbow.jpg');
+    mid1= loadImage('images/egg.jpg');
+    mid2= loadImage('images/tiger.jpg');
+    mid3= loadImage('images/lanaface.jpg');
+    fr1= loadImage('images/lanaface2.jpg');
+    fr2= loadImage('images/moneysleep.jpg');
+    fr3= loadImage('images/girls.jpg');
+
+
+  
+  }
+  
+  function draw() {
+      
+    image(bg1,0,0);
+    
+
+
+    
+    
+       
+  }
